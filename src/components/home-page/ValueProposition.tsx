106ec45c16cf36e2dd30544bac60f6cd5e5b611f
Cristian Serrón <cristian99ser@gmail.com>
2026-06-12 12:14:33 -0300

    Added ValueProposition.tsx

--- src/components/home-page/ValueProposition.tsx
+++ src/components/home-page/ValueProposition.tsx
@@ -1,10 +1,10 @@
 function ValueProposition() {
   return (
-    <div className="flex flex-col items-center gap-5 py-8 bg-amber-300">
+    <div className="flex flex-col gap-5 py-8">
       <h2 className="text-2xl/snug font-medium text-balance text-center">
         Always know where you stand financially.
       </h2>
-      <p className="text-balance">Open the app and instantly understand your balance, your spending, and how much room you have to make your next financial move with confidence.</p>
+      <p className="text-balance text-center">Open the app and instantly understand your balance, your spending, and how much room you have to make your next financial move with confidence.</p>
     </div>
   );
 }
