5485793ce890bb7681207bc1c34862e4df6ecf32
Elix <byelixlab@gmail.comlear>
2026-06-14 08:13:38 -0300

    Finished stylings and responsive design for Stakes.tsx

--- src/components/home-page/Stakes.tsx
+++ src/components/home-page/Stakes.tsx
@@ -8,12 +8,12 @@ const stakes = [
 function Stakes() {
   return (
     <div className="flex flex-col items-center gap-5 py-12 px-4">
-      <h2 className="text-2xl font-bold text-center text-balance lg:text-3xl xl:text-4xl max-w-6xl">
+      <h2 className="max-w-3xl text-2xl font-bold text-center text-balance lg:text-3xl xl:text-4xl lg:max-w-5xl">
         Money becomes harder to manage when you don&apos;t know where it&apos;s going.
       </h2>
-      <ul className="grid grid-cols-2 gap-5 w-6xl">
+      <ul className="flex flex-col gap-3 max-w-6xl lg:grid lg:grid-cols-2">
         {stakes.map((stake,index) => (
-          <li key={index} className="border border-green-200 shadow-md shadow-green-100 rounded-lg font-medium text-center p-5 hover:bg-green-50">
+          <li key={index} className="border border-gray-300 shadow-md shadow-gray-100 rounded-lg font-medium text-center p-3 bg-background lg:p-5 hover:bg-red-100 transition-all">
             {stake}
           </li>
         ))}
