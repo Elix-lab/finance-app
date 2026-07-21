07c54fa00514f86e8dce93cc0380e834924dc6d2
Cristian Serrón <cristian99ser@gmail.com>
2026-07-21 00:37:30 -0300

    Changed background colors

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,23 +1,31 @@
-import { getFinanceSummaryAction, getLatestTxAction } from "@/_actions/transactions/get";
+import {
+  getFinanceSummaryAction,
+  getLatestTxAction,
+} from "@/_actions/transactions/get";
 import TransactionButton from "../../components/transaction/TransactionButton";
 import TransactionTable from "@/components/transaction/TransactionTable";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { getQueryClient } from "@/lib/getQueryClient";
 import Balance from "@/components/dashboard/Balance";
 
-
 const Page = async () => {
   const queryClient = getQueryClient();
 
   await Promise.all([
-    queryClient.prefetchQuery({queryKey: ["finance-summary"],queryFn: () => getFinanceSummaryAction(),}),
-    queryClient.prefetchQuery({queryKey: ["transactions", 'latest'], queryFn: () => getLatestTxAction(),}),
-  ])
+    queryClient.prefetchQuery({
+      queryKey: ["finance-summary"],
+      queryFn: () => getFinanceSummaryAction(),
+    }),
+    queryClient.prefetchQuery({
+      queryKey: ["transactions", "latest"],
+      queryFn: () => getLatestTxAction(),
+    }),
+  ]);
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <div className="flex justify-center px-4">
-        <div className="flex flex-col gap-3 w-full lg:max-w-6xl">
+      <div className="flex justify-center">
+        <div className="flex flex-col gap-5 w-full max-w-3xl px-4 sm:px-6 pt-6 sm:pt-10">
           {/* Balance summary component*/}
           <Balance />
           {/* Buttons to manage income and expenses */}
