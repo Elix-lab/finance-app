013ed13980e9c13f9eeb52401c023b0d126f65e3
Cristian Serrón <cristian99ser@gmail.com>
2026-08-19 22:18:25 -0300

    Deleted getParams.tsx

--- src/lib/getParams.ts
+++
@@ -1,7 +0,0 @@
-'use client'
-export const getParams = () => {
-    // Getting URL params
-      const searchParams = useSearchParams();
-      const params = new URLSearchParams(searchParams);
-      const router = useRouter();
-}
